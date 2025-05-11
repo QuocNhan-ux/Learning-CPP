98c0046b1c48cc77db9e0f904850cb808036b077
Quoc Nhan <qoucnhantra@gmail.com>
2025-05-11 11:34:04 -0500

    updated users.cpp

--- users.cpp
+++ users.cpp
@@ -1,5 +1,7 @@
 #include "users.h"
 #include <iostream>
 
-Users::Users(std::string name, int ID, std::string user, std::string pass) 
-: name(name), patronID(ID), username(user), password(pass), fineBalance(0.0)  {}
+Users::Users(std::string nm, int ID, std::string user, std::string pass)
+: name(nm), patronID(ID), username(user), password(pass), fineBalance(0.0), numBooks(0), numCDs(0)  {}
+
+Users::Users() : patronID(0), fineBalance(0.0), numBooks(0), numCDs(0) {}
